bfb182adc201279b0350789e2784738d22327c5b
Alejandro <eloso85@gmail.com>
2020-12-19 11:50:18 -0600

    made navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
-import Products from './components/Products/Products'
+// import Products from './components/Products/Products'
+// import NavBar from './components/Navbar/NavBar'
+
+import {Products, Navbar} from './components'
 
 const App = () => {
     return (
         <div>
+            <Navbar/>
             <Products/>
         </div>
     )
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export {default as Navbar} from './Navbar/NavBar';
+export {default as Products} from './Products/Products'
